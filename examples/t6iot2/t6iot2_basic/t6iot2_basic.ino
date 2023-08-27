e6ec302a82c76919c96e5cf7628e5a61f16b996b
mathcoll <mathieu@internetcollaboratif.info>
2023-08-27 15:08:44 +0200

    update boards tests

--- examples/t6iot2/t6iot2_basic/t6iot2_basic.ino
+++ examples/t6iot2/t6iot2_basic/t6iot2_basic.ino
@@ -11,7 +11,8 @@
 
    ESP32 & ESP8266 known compatible architectures:
    - ESP32-WROOM-DA module          / Minimal SPIFFS 1.9MB APP with OTA/190KB SPIFFS
-   - NodeMCU 1.0 (ESP-12E) Module
+   - NodeMCU 1.0 (ESP-12E) Module // Using nodemcu board v2.7.4: https://github.com/arrowhead-f/ArrowheadESP/issues/6#issuecomment-865923278
+   - WEMOS D1 R32
 */
 
 #include <t6iot.h>
