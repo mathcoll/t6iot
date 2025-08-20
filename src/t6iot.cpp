02fc6861b07111459abbb7f78719ab5c6542f6a5
mathcoll <mathieu@internetcollaboratif.info>
2025-08-20 19:26:15 +0200

    v2.0.17

--- src/t6iot.cpp
+++ src/t6iot.cpp
@@ -9,9 +9,9 @@
 // nmap --script ssl-cert.nse -p 443 api.internetcollaboratif.info | grep SHA-1
 // openssl s_client -connect api.internetcollaboratif.info:443 -prexit -showcerts -state -status -tlsextdebug -verify 10
 
-String VERSION = "2.0.15";
+String VERSION = "2.0.17";
 
-const char *fingerprint = "5310 330b c70c 7bae af26 b344 b78b 0f0f 33e1 1f77";
+const char *fingerprint = "4a51 2f5e 9291 e0ab c05b c811 8941 15b1 3d4c d607";
 const char* root_ca PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
 MIIDejCCAmKgAwIBAgIQf+UwvzMTQ77dghYQST2KGzANBgkqhkiG9w0BAQsFADBX
